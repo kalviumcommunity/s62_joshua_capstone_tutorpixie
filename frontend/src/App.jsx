ed1a68efc57aaa5cc31d4bb4a556c9a14629b3ce
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2025-02-11 12:49:34 +0530

    added basic pages and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Blog from './pages/Blog'
+import Layout from './pages/Layout'
 
 function App() {
   return (
-    <div>
-      Tutorpixie
-    </div>
+    <Routes>
+      <Route element={<Layout/>}>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/blog" element={<Blog/>}/>
+      </Route>
+    </Routes>
   )
 }
 
