87798678df16f616fedbbb2ca64936789543fd5b
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2025-02-11 14:19:06 +0530

    added Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import Contact from './pages/Contact'
 import Login from './pages/Login'
 import Blog from './pages/Blog'
 import Layout from './pages/Layout'
+import Pricing from './pages/Pricing'
+import Demo from './pages/Demo'
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path="/contact" element={<Contact/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/blog" element={<Blog/>}/>
+        <Route path="/pricing" element={<Pricing/>}/>
+        <Route path="/demo" element={<Demo/>}/>
       </Route>
     </Routes>
   )
---
+++ frontend/src/pages/Demo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Demo() {
+  return (
+    <div>
+      Demo Form
+    </div>
+  )
+}
+
+export default Demo
