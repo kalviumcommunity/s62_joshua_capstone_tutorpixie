ea8f7680db483c3f4afb6f6d4dee2c5116949913
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2025-02-27 10:13:57 +0530

    added POST request

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -19,3 +19,24 @@ export async function GET(){
         return NextResponse.json({message: "Internal Server Error", success: false})
     }
 }
+
+export async function POST(req: Request) {
+    try {
+        const {email, password} = await req.json();
+        if(!email || !password){
+            return NextResponse.json({message: "Email and password are required", success: false}, {status: 400});
+        }
+
+        const userExists = await prisma.user.findUnique({where: {email}});
+        if(userExists){
+            return NextResponse.json({message: "user already exists"})
+        }
+
+        const user = await prisma.user.create({data: {email: email, password: password},});
+
+        return NextResponse.json(user);
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({message: "Internal Server Error", success: false});
+    }
+}
