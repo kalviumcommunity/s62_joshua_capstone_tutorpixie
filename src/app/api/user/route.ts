a0b4755f962dec7fc20a9afdf46443b3b88539c7
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2025-02-27 10:08:53 +0530

    refactored according to review

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -4,9 +4,18 @@ import {prisma} from '@/lib/db'
 export async function GET(){
     try {
         const users = await prisma.user.findMany()
+
+        if (!users || users.length === 0) {
+            return NextResponse.json({
+                message: "No users found",
+                data: [],
+                success: true, 
+            });
+        }
+
         return NextResponse.json({message: "Users fetched", data:users, success: true})
     } catch (error) {
-        console.log(error)
+        console.error(error)
         return NextResponse.json({message: "Internal Server Error", success: false})
     }
 }
