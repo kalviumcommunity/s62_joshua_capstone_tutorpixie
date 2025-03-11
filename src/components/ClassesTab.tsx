c8dc3bed51ab467392d28e5c9e3317b07fe3886d
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2025-03-11 23:18:37 +0530

    card classesTab more dynamic

--- src/components/ClassesTab.tsx
+++ src/components/ClassesTab.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import ClassCard from "./ClassCard";
 
-const ClassesTab: React.FC<{name: string, type: string}> = ({name, type='confirmed'}) => {
+const ClassesTab: React.FC<{name: string, type: string, classList: {subject: string, time: string, date: string}[]}> = ({name, type='confirmed', classList= [{subject: "Physics", time: "9-10pm", date: "Sat 3rd Feb", type: "confirmed"},
+    {subject: "Math", time: "9-10pm", date: "Sat 3rd Feb", type: "confirmed"},
+    {subject: "English", time: "9-10pm", date: "Sat 3rd Feb", type: "confirmed"}]}) => {
+
   return (
     <div className="bg-gray-800 text-white p-6 rounded-lg min-w-120 my-4 max-w-md">
       <h2 className="text-xl font-bold mb-4">{name}</h2>
       <div className="space-y-4 max-h-90 overflow-auto">
-        <ClassCard subject="Math" time="9-10pm" date="Sat 3rd Feb" type={type} />
-        <ClassCard subject="Physics" time="9-10pm" date="Sat 3rd Feb" type={type} />
-        <ClassCard subject="Physics" time="9-10pm" date="Sat 3rd Feb" type={type} />
-        <ClassCard subject="Physics" time="9-10pm" date="Sat 3rd Feb" type={type} />
-        <ClassCard subject="Physics" time="9-10pm" date="Sat 3rd Feb" type={type} />
-        <ClassCard subject="Physics" time="9-10pm" date="Sat 3rd Feb" type={type} />
-        <ClassCard subject="Physics" time="9-10pm" date="Sat 3rd Feb" type={type} />
-        <ClassCard subject="Physics" time="9-10pm" date="Sat 3rd Feb" type={type} />
-        <ClassCard subject="Physics" time="9-10pm" date="Sat 3rd Feb" type={type} />
+        {
+            classList.map((item, index) => (
+                <ClassCard key={index} subject={item.subject} time={item.time} date={item.date} type={type} />
+            ))
+        };
       </div>
     </div>
   );
