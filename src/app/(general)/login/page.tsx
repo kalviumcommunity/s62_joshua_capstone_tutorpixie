057ca799c86634013048b99318cc7feda0b6e98a
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2025-06-27 15:49:14 +0530

    login page frontend update

--- src/app/(general)/login/page.tsx
+++ src/app/(general)/login/page.tsx
@@ -68,13 +68,13 @@ export default function LogIn() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <Head>
-        <title>Sign Up</title>
-        <meta name="description" content="Create an account" />
+        <title>Login</title>
+        <meta name="description" content="Log in to your account" />
       </Head>
 
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Create an Account
+          Login
         </h2>
       </div>
 
